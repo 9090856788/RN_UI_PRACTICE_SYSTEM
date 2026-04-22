bc5047f128edaed9420ffa837d5db12caef71d0d
kanhucharan <kanhucharansahoo595@gmail.com>
2026-04-22 19:18:50 +0530

    do the initial setup & install required dependency

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
 
-const App = () => {
+export default function App() {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>RN UI Mastery 🚀</Text>
     </View>
-  )
+  );
 }
-
-export default App
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin', // ⚠️ MUST be last
+  ],
 };
